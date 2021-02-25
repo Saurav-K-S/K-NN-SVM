617e37a747d8c22038a00808a16b11cb1e6ad2bb
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2021-02-25 04:08:59 -0800

    Update KNNSVM.py

--- KNNSVM.py
+++ KNNSVM.py
@@ -20,9 +20,9 @@ from sklearn.model_selection import train_test_split
 x_tr ,x_te, y_tr, y_te = train_test_split(x, y, test_size = 0.20, random_state = 0)
 
 
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors = 10, metric = 'minkowski', p=2)
-classifier.fit(x_tr,y_tr)
+#from sklearn.neighbors import KNeighborsClassifier
+#classifier = KNeighborsClassifier(n_neighbors = 10, metric = 'minkowski', p=2)
+#classifier.fit(x_tr,y_tr)
 
 #from sklearn.svm import SVC
 #classifier = SVC(kernel = 'linear', random_state = 0)
